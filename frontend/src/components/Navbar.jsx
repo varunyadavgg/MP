75a9895586f8ae9541dcd1e947b865529c0de899
varunyadavgg <zvarun747@gmail.com>
2022-10-08 17:53:50 +0530

    changes to the navbaar
    this is a css navbar application resolve

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,12 +4,15 @@ import './navbar.css';
 
 function Navbar(){
 
-    return <nav className="navbar bg-dark container">
-        <h4><Link className="LINK" to="/">  home</Link></h4>
-        <h4><Link className="LINK" to="/events"> events</Link></h4>
-        <h4><Link className="LINK" to="/create">  create   </Link></h4>
+    return <div><nav>
+        <a><img className="logo" src="https://p.kindpng.com/picc/s/765-7657079_download-logo-twitter-computer-black-icons-free-transparent.png"></img></a>
+        <a ><h4><Link className="LINK" to="/">  home</Link></h4></a>
+        <a ><h4><Link className="LINK" to="/events"> events</Link></h4></a>
+        <a><h4><Link className="LINK" to="/create">  create   </Link></h4></a>
+        <div className="animation start-home"></div>
         
     </nav>
+    </div>
 }
 
 export default Navbar;
